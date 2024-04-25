cf8878f89f957416ad454c1aa56101d2116c23d2
nerwinalamas <nerwinalamas@gmail.com>
2024-04-25 21:36:21 +0800

    socket for end call

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -4,55 +4,70 @@ const app = express();
 const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin: ["http://localhost:5173"],
-        methods: ["GET", "POST"],
-    }
+	cors: {
+		origin: ["http://localhost:5173"],
+		methods: ["GET", "POST"],
+	},
 });
 
 const findRecipientSocketId = (userId) => {
-    return userSocketMap[userId];
+	return userSocketMap[userId];
 };
 
 const userSocketMap = {};
 const userCameraState = {};
 
-io.on("connection", function(socket) {
-    socket.on("userConnected", function(userId) {
-        userSocketMap[userId] = socket.id;
-    });
+io.on("connection", (socket) => {
+	socket.on("userConnected", (userId) => {
+		userSocketMap[userId] = socket.id;
+	});
 
-    socket.on("message", function(data) {
-        try {
-            io.emit("mensahe", data);
-        } catch (error) {
-            console.error("Error handling message:", error);
-        }
-    });
+	socket.on("message", (data) => {
+		try {
+			io.emit("mensahe", data);
+		} catch (error) {
+			console.error("Error handling message:", error);
+		}
+	});
 
-    socket.on("disconnect", function() {
-        const userId = Object.keys(userSocketMap).find(key => userSocketMap[key] === socket.id);
-        if (userId) {
-            delete userSocketMap[userId];
-            delete userCameraState[socket.id];
-            socket.broadcast.emit("camera_state_change", { userId: socket.id, newState: false }); 
-        }
-    });
+	socket.on("disconnect", () => {
+		const userId = Object.keys(userSocketMap).find(
+			(key) => userSocketMap[key] === socket.id
+		);
+		if (userId) {
+			delete userSocketMap[userId];
+			delete userCameraState[socket.id];
+			socket.broadcast.emit("camera_state_change", {
+				userId: socket.id,
+				newState: false,
+			});
+		}
+	});
 
-    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-        // io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-        io.emit("callUser", { signal: signalData, from, name });
-    });
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		// io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+		io.emit("callUser", { signal: signalData, from, name });
+		console.log("callUser: ", { userToCall, signalData, from, name });
+	});
 
-    socket.on("answerCall", (data) => {
-        // io.to(data.to).emit("callAccepted", data.signal)
-        io.emit("callAccepted", data.signal)
-    });
+	socket.on("answerCall", (data) => {
+		// io.to(data.to).emit("callAccepted", data.signal)
+		io.emit("callAccepted", data.signal);
+		console.log("answerCall: ", data);
+	});
 
-    socket.on("camera_state_change", (newState) => {
-        userCameraState[socket.id] = newState;
-        socket.broadcast.emit("camera_state_change", { userId: socket.id, newState });
-      });
+	socket.on("camera_state_change", (newState) => {
+		userCameraState[socket.id] = newState;
+		socket.broadcast.emit("camera_state_change", {
+			userId: socket.id,
+			newState,
+		});
+	});
+
+	socket.on("endCall", () => {
+		io.emit("callEnded");
+		console.log("Call ended");
+	});
 });
 
 module.exports = { app, io, server };
