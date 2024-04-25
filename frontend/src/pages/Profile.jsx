5bed694c38b6ab110716db9daefab0f821c74ced
nerwinalamas <nerwinalamas@gmail.com>
2024-04-25 21:34:44 +0800

    minor changes

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,35 +1,39 @@
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { savedPosts, unsavedPosts } from "../_actions/userAction";
-import { likePost, setClickedPost, unlikePost } from "../_actions/postsAction";
+import {
+	likeOtherPost,
+	savedOtherPosts,
+	setClickedOtherPost,
+	setOtherUser,
+	setOtherUserPosts,
+	unlikeOtherPost,
+	unsavedOtherPosts,
+} from "../_actions/otherUserAction";
+import axios from "axios";
 
 import PostModal from "../components/PostModal";
 import PostReaction from "../components/PostReaction";
 import PostUserInfo from "../components/PostUserInfo";
 import UserProfile from "../components/UserProfile";
 
-import axios from "axios";
-import { likeOtherPost, savedOtherPosts, setClickedOtherPost, setOtherUser, setOtherUserPosts, unlikeOtherPost, unsavedOtherPosts } from "../_actions/otherUserAction";
-import { Bookmark, MessageCircle, Redo2, ThumbsUp } from "lucide-react";
 
 const Profile = () => {
 	const { id } = useParams();
 
 	// otherUser
-	const user = useSelector((state) => state.otherUserReducer.otherUser)
-	const otherUserPosts = useSelector((state) => state.otherUserReducer.otherUserPosts)
-	const clickedOtherPost = useSelector((state) => state.otherUserReducer.clickedOtherPost);
-
-	// console.log("otherUser: ", otherUser)
-	// console.log("otherUserPosts: ", otherUserPosts)
-
-	// console.log("user: ", user)
+	const user = useSelector((state) => state.otherUserReducer.otherUser);
+	const otherUserPosts = useSelector(
+		(state) => state.otherUserReducer.otherUserPosts
+	);
+	const clickedOtherPost = useSelector(
+		(state) => state.otherUserReducer.clickedOtherPost
+	);
 
 	const currentUser = useSelector((state) => state.userReducer.user);
 	// const clickedPost = useSelector((state) => state.postReducer.clickedPost);
-	const theme = useSelector((state) => state.themeReducer.theme)
+	const theme = useSelector((state) => state.themeReducer.theme);
 	const dispatch = useDispatch();
 
 	const getUser = async (userId) => {
@@ -44,7 +48,7 @@ const Profile = () => {
 					},
 				}
 			);
-			dispatch(setOtherUser(response.data.data))
+			dispatch(setOtherUser(response.data.data));
 		} catch (error) {
 			console.log("Get User Profile Error: ", error);
 		}
@@ -66,7 +70,7 @@ const Profile = () => {
 					},
 				}
 			);
-			dispatch(setOtherUserPosts(response.data.data))
+			dispatch(setOtherUserPosts(response.data.data));
 		} catch (error) {
 			console.log("Get Posts Error: ", error);
 		}
@@ -75,7 +79,6 @@ const Profile = () => {
 	useEffect(() => {
 		getPosts(id);
 	}, [otherUserPosts, dispatch, user._id]);
-
 
 	const handleClick = (post) => {
 		dispatch(setClickedOtherPost(post));
@@ -102,7 +105,10 @@ const Profile = () => {
 					(likeId) => likeId !== user._id
 				);
 				dispatch(
-					setClickedOtherPost({ ...clickedOtherPost, likes: updatedLikes })
+					setClickedOtherPost({
+						...clickedOtherPost,
+						likes: updatedLikes,
+					})
 				);
 			} else {
 				const response = await axios.post(
@@ -129,7 +135,7 @@ const Profile = () => {
 			console.log("Like Posts Error: ", error);
 		}
 	};
-	
+
 	const handleSavePost = async (postId, isSaved) => {
 		try {
 			if (isSaved) {
@@ -190,7 +196,11 @@ const Profile = () => {
 					otherUserPosts.map((post) => (
 						<div
 							key={post._id}
-							className={`w-80 p-3  rounded-md flex flex-col gap-2 md:w-96 md:gap-5 lg:p-5 xl:w-[600px] xl:gap-1 ${theme === "dark" ? "bg-customGray" : "bg-slate-100" }`}
+							className={`w-80 p-3  rounded-md flex flex-col gap-2 md:w-96 md:gap-5 lg:p-5 xl:w-[600px] xl:gap-1 ${
+								theme === "dark"
+									? "bg-customGray"
+									: "bg-slate-100"
+							}`}
 						>
 							{/* USER INFO */}
 							<PostUserInfo post={post} user={user} />
@@ -198,7 +208,11 @@ const Profile = () => {
 							<img
 								src={`http://localhost:5000/uploads/${post.postPicture}`}
 								alt="Sample Image"
-								className={`xl:max-h-[500px] object-contain rounded-md ${theme === "dark" ? "bg-customBlack" : "bg-slate-200" }`}
+								className={`xl:max-h-[500px] object-contain rounded-md ${
+									theme === "dark"
+										? "bg-customBlack"
+										: "bg-slate-200"
+								}`}
 							/>
 							{/* REACTION SECTION */}
 
