72ef538b2e60c49f37fa4d82b60d7edcf729ad56
nerwinalamas <nerwinalamas@gmail.com>
2024-05-27 13:23:14 +0800

    feat: like and unlike post mutation

--- frontend/src/mutation/post.js
+++ frontend/src/mutation/post.js
@@ -1,11 +1,29 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createPost } from "../api/post";
+import { createPost, likePost, unlikePost } from "../api/post";
 
 export const useCreatePostMutation = () => {
 	const queryClient = useQueryClient();
 
 	return useMutation({
 		mutationFn: ({ postContent, postPicture }) => createPost(postContent, postPicture),
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["posts"] }),
+	});
+};
+
+export const useLikePostMutation = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: likePost,
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["posts"] }),
+	});
+};
+
+export const useUnLikePostMutation = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+        mutationFn: unlikePost,
 		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["posts"] }),
 	});
 };
