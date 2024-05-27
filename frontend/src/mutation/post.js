720b17a39b6e9de64c95e1dc57ed5dcaf6ee631b
nerwinalamas <nerwinalamas@gmail.com>
2024-05-27 14:18:21 +0800

    feat: save and unsave post mutation

--- frontend/src/mutation/post.js
+++ frontend/src/mutation/post.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createPost, likePost, unlikePost } from "../api/post";
+import { createPost, likePost, savePost, unlikePost, unsavePost } from "../api/post";
 
 export const useCreatePostMutation = () => {
 	const queryClient = useQueryClient();
@@ -24,6 +24,24 @@ export const useUnLikePostMutation = () => {
 
 	return useMutation({
         mutationFn: unlikePost,
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["posts"] }),
+	});
+};
+
+export const useSavePostMutation = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+        mutationFn: savePost,
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["posts"] }),
+	});
+};
+
+export const useUnsavePostMutation = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+        mutationFn: unsavePost,
 		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["posts"] }),
 	});
 };
