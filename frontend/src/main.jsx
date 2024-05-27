f44a52360f56422df2f5662d6f1a45f08397c313
nerwinalamas <nerwinalamas@gmail.com>
2024-05-27 12:37:56 +0800

    adding tanstack query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,18 @@ import "./index.css";
 import store from "./_store/store.jsx";
 import { Provider } from "react-redux";
 import { SocketProvider } from "./context/SocketContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<SocketProvider>
-				<App />
-			</SocketProvider>
-		</Provider>
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<SocketProvider>
+					<App />
+				</SocketProvider>
+			</Provider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
