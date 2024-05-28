00be2b27607983e5160b43bc661d9c0e3035e013
nerwinalamas <nerwinalamas@gmail.com>
2024-05-28 10:22:36 +0800

    feat: search user mutation

--- frontend/src/mutation/user.js
+++ frontend/src/mutation/user.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { updateProfilePhoto } from "../api/user";
+import { searchUser, updateProfilePhoto } from "../api/user";
 
 export const useUpdateProfilePhotoMutation = () => {
 	const queryClient = useQueryClient();
@@ -7,5 +7,14 @@ export const useUpdateProfilePhotoMutation = () => {
 	return useMutation({
         mutationFn: ({ userId, profilePicture }) => updateProfilePhoto(userId, profilePicture),
 		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["posts", "userPosts"] }),
+	});
+};
+
+export const useSearchUserMutation = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+        mutationFn: searchUser,
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["search"] }),
 	});
 };
