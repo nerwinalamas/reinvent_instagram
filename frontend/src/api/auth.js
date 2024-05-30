685436175d4c40a425e08e3ee79e7d63d8338456
nerwinalamas <nerwinalamas@gmail.com>
2024-05-30 16:40:10 +0800

    feat: register api

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -13,3 +13,25 @@ export const loginUser = async (email, password) => {
 		console.log("Login Error: ", error);
 	}
 };
+
+export const registerUser = async (
+	email,
+	firstName,
+	lastName,
+	userName,
+	password
+) => {
+	try {
+		const response = await axios.post(API.REGISTER, {
+			email,
+			firstName,
+			lastName,
+			userName,
+			password,
+		});
+
+		return response.data;
+	} catch (error) {
+		console.log("Registration Error: ", error);
+	}
+};
