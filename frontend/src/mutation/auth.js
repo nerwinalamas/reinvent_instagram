0bd6e64cd66f6845e0ed27eef95239439301acd9
nerwinalamas <nerwinalamas@gmail.com>
2024-05-30 16:40:23 +0800

    feat: register mutation

--- frontend/src/mutation/auth.js
+++ frontend/src/mutation/auth.js
@@ -1,11 +1,21 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { loginUser } from "../api/auth";
+import { loginUser, registerUser } from "../api/auth";
 
 export const useLoginUserMutation = () => {
 	const queryClient = useQueryClient();
 
 	return useMutation({
-        mutationFn: ({ email, password }) => loginUser(email, password),
+		mutationFn: ({ email, password }) => loginUser(email, password),
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["user"] }),
+	});
+};
+
+export const useRegisterUserMutation = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: ({ email, firstName, lastName, userName, password }) =>
+			registerUser(email, firstName, lastName, userName, password),
 		onSuccess: () => queryClient.invalidateQueries({ queryKey: ["user"] }),
 	});
 };
