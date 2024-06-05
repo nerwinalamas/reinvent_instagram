baccb43fc2db7dca5ddbe24d92e083a8d71506bc
nerwinalamas <nerwinalamas@gmail.com>
2024-06-05 15:55:20 +0800

    refactor: login api

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -2,36 +2,30 @@ import axios from "axios";
 import { API } from "../constants/endpoints";
 
 export const loginUser = async (email, password) => {
-	try {
-		const response = await axios.post(API.LOGIN, {
-			email,
-			password,
-		});
-
-		return response.data;
-	} catch (error) {
-		console.log("Login Error: ", error);
-	}
+    return await axios.post(API.LOGIN, {
+        email,
+        password,
+    });
 };
 
 export const registerUser = async (
-	email,
-	firstName,
-	lastName,
-	userName,
-	password
+    email,
+    firstName,
+    lastName,
+    userName,
+    password
 ) => {
-	try {
-		const response = await axios.post(API.REGISTER, {
-			email,
-			firstName,
-			lastName,
-			userName,
-			password,
-		});
+    try {
+        const response = await axios.post(API.REGISTER, {
+            email,
+            firstName,
+            lastName,
+            userName,
+            password,
+        });
 
-		return response.data;
-	} catch (error) {
-		console.log("Registration Error: ", error);
-	}
+        return response.data;
+    } catch (error) {
+        console.log("Registration Error: ", error);
+    }
 };
