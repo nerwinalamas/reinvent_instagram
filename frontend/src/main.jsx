fe65eabc65a72f9a2d1ffebb75089ad540824040
nerwinalamas <nerwinalamas@gmail.com>
2024-06-10 15:54:34 +0800

    refactor: remove redux in main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,21 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import store from "./_store/store.jsx";
-import { Provider } from "react-redux";
 import { SocketProvider } from "./context/SocketContext.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<Provider store={store}>
-				<SocketProvider>
-					<App />
-				</SocketProvider>
-			</Provider>
-		</QueryClientProvider>
-	</React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <SocketProvider>
+                <App />
+            </SocketProvider>
+        </QueryClientProvider>
+    </React.StrictMode>
 );
